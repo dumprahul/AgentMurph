96402cb00e33a831332431be89a170243a141f41
dumprahul <shanrahul04@gmail.com>
2025-02-15 06:22:07 +0530

    tools-init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import 'dotenv/config';
 import OpenAI from "openai";
+import { createAssistant } from './openai/createAssistant.js';
+import { createThread } from './openai/createThread.js';
+import { createRun } from './openai/createRun.js';
+import { performRun } from './openai/performRun.js';
 
 async function main(){
 
     const client = new OpenAI();
+    const message = "Hello I'm Agent Murph!"
     const assistant = await createAssistant(client);
+    const thread = await createThread(client,message);
 
-}
+    const run = await createRun(client, thread, assistant.id);
+    const result = await performRun(run, client, thread);
+};
+
+main();
+
